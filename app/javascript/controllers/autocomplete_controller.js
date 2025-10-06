5114000e9a1392d42c91084f23e423061b634435
tomszociepa <tom.ociepa@gmail.com>
2025-10-06 10:20:27 +0200

    add mapbox to form

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="autocomplete"
+// Stimulus controller dla Mapbox Search
 export default class extends Controller {
   static targets = [
     "pickup",
@@ -12,113 +12,18 @@ export default class extends Controller {
   ];
 
   connect() {
-    this.isPickupValid = false;
-    this.isDeliveryValid = false;
+    // Pickup
+    this.pickupTarget.addEventListener("retrieve", (e) => {
+      const feature = e.detail.features[0];
+      this.pickupLatTarget.value = feature.geometry.coordinates[1];
+      this.pickupLonTarget.value = feature.geometry.coordinates[0];
+    });
 
-    if (typeof google === "undefined") {
-      document.addEventListener(
-        "google-maps-loaded",
-        () => {
-          this.initAutocomplete();
-        },
-        { once: true }
-      );
-    } else {
-      this.initAutocomplete();
-    }
-  }
-
-  initAutocomplete() {
-    // --- Pickup Autocomplete ---
-    if (this.hasPickupTarget) {
-      const pickupAutocomplete = new google.maps.places.Autocomplete(
-        this.pickupTarget
-      );
-      pickupAutocomplete.addListener("place_changed", () => {
-        const place = pickupAutocomplete.getPlace();
-        if (place.geometry) {
-          this.pickupLatTarget.value = place.geometry.location.lat();
-          this.pickupLonTarget.value = place.geometry.location.lng();
-          this.isPickupValid = true;
-          this.clearFieldError(this.pickupTarget);
-        } else {
-          this.isPickupValid = false;
-        }
-      });
-      this.pickupTarget.addEventListener("input", () => {
-        this.isPickupValid = false;
-      });
-    }
-
-    // --- Delivery Autocomplete ---
-    if (this.hasDeliveryTarget) {
-      const deliveryAutocomplete = new google.maps.places.Autocomplete(
-        this.deliveryTarget
-      );
-      deliveryAutocomplete.addListener("place_changed", () => {
-        const place = deliveryAutocomplete.getPlace();
-        if (place.geometry) {
-          this.deliveryLatTarget.value = place.geometry.location.lat();
-          this.deliveryLonTarget.value = place.geometry.location.lng();
-          this.isDeliveryValid = true;
-          this.clearFieldError(this.deliveryTarget);
-        } else {
-          this.isDeliveryValid = false;
-        }
-      });
-      this.deliveryTarget.addEventListener("input", () => {
-        this.isDeliveryValid = false;
-      });
-    }
-
-    // --- Form validation ---
-    const form = this.element.querySelector("form");
-    if (form) {
-      form.addEventListener("submit", (event) => {
-        let firstError = null;
-
-        if (!this.isPickupValid) {
-          event.preventDefault();
-          this.showFieldError(
-            this.pickupTarget,
-            "Proszę wybrać poprawny adres odbioru z sugestii."
-          );
-          if (!firstError) firstError = this.pickupTarget;
-        } else {
-          this.clearFieldError(this.pickupTarget);
-        }
-
-        if (!this.isDeliveryValid) {
-          event.preventDefault();
-          this.showFieldError(
-            this.deliveryTarget,
-            "Proszę wybrać poprawny adres dostawy z sugestii."
-          );
-          if (!firstError) firstError = this.deliveryTarget;
-        } else {
-          this.clearFieldError(this.deliveryTarget);
-        }
-
-        if (firstError) {
-          firstError.scrollIntoView({ behavior: "smooth", block: "center" });
-          firstError.focus();
-        }
-      });
-    }
-  }
-
-  showFieldError(input, message) {
-    this.clearFieldError(input);
-    const error = document.createElement("div");
-    error.className = "invalid-feedback d-block";
-    error.textContent = message;
-    input.parentNode.appendChild(error);
-    input.classList.add("is-invalid");
-  }
-
-  clearFieldError(input) {
-    const error = input.parentNode.querySelector(".invalid-feedback");
-    if (error) error.remove();
-    input.classList.remove("is-invalid");
+    // Delivery
+    this.deliveryTarget.addEventListener("retrieve", (e) => {
+      const feature = e.detail.features[0];
+      this.deliveryLatTarget.value = feature.geometry.coordinates[1];
+      this.deliveryLonTarget.value = feature.geometry.coordinates[0];
+    });
   }
 }
