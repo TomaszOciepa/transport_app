a66381699d6244cd6caecc9f055a383664cff8a0
tomszociepa <tom.ociepa@gmail.com>
2025-10-07 18:59:16 +0200

    add style for flash messages

---
+++ app/javascript/controllers/flash.js
@@ -0,0 +1,12 @@
+// app/javascript/controllers/flash.js
+import * as bootstrap from "bootstrap";
+
+document.addEventListener("turbo:load", () => {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => {
+    setTimeout(() => {
+      const bsAlert = bootstrap.Alert.getOrCreateInstance(alert);
+      bsAlert.close();
+    }, 3000);
+  });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,9 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.bundle.min.js"
 
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+pin "flash", to: "controllers/flash.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/index.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.esm.min.js"
+
