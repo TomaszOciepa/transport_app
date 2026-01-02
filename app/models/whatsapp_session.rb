038bd72d72ec35beb68f9236da44aa28e2ff4251
Tomasz Ociepa <mangi@MacBook-Air-Tomasz.local>
2026-01-02 21:22:33 +0100

    dynamic update of the connection with WhatsApp

--- app/models/whatsapp_session.rb
+++ app/models/whatsapp_session.rb
@@ -1,3 +1,16 @@
 class WhatsappSession < ApplicationRecord
   belongs_to :user
+  after_update_commit :broadcast_changes
+
+  private
+
+  def broadcast_changes
+    Rails.logger.info "🔥 BROADCAST WhatsappSession #{id} STATUS=#{status}"
+    broadcast_replace_to(
+      "whatsapp_session_#{user_id}",
+      target: "whatsapp_session",
+      partial: "messages/whatsapp_session",
+      locals: { whatsapp_session: self }
+    )
+  end
 end
